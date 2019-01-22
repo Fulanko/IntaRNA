915f256978d19e00523f680e5ca6bb4ed3ddf4da
Frank <frank.gelhausen@gmail.com>
2019-01-22 16:13:11 +0100

    Implemented PredictorMfeEns2dHeuristicSeedExtension

--- src/bin/CommandLineParsing.cpp
+++ src/bin/CommandLineParsing.cpp
@@ -49,6 +49,7 @@ extern "C" {
 #include "IntaRNA/PredictorMfe2dSeedExtension.h"
 #include "IntaRNA/PredictorMfeEns2dSeedExtension.h"
 #include "IntaRNA/PredictorMfe2dHeuristicSeedExtension.h"
+#include "IntaRNA/PredictorMfeEns2dHeuristicSeedExtension.h"
 
 #include "IntaRNA/PredictionTracker.h"
 #include "IntaRNA/PredictionTrackerHub.h"
@@ -2033,6 +2034,7 @@ getPredictor( const InteractionEnergy & energy, OutputHandler & output ) const
 			case 'X' :  return new PredictorMfe2dSeedExtension( energy, output, predTracker, getSeedHandler( energy ) );
 			case 'Y' :  return new PredictorMfe2dHeuristicSeedExtension( energy, output, predTracker, getSeedHandler( energy ) );
 			case 'A' :  return new PredictorMfeEns2dSeedExtension( energy, output, predTracker, getSeedHandler( energy ) );
+			case 'B' :  return new PredictorMfeEns2dHeuristicSeedExtension( energy, output, predTracker, getSeedHandler( energy ) );
 			}
 		} break;
 		// single-site max-prob interactions (contain only interior loops)
