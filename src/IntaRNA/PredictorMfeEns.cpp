81550195ac36c645d204dd6e5f3e986188062e71
martin-mann <qyu@gmx.de>
2019-05-02 18:33:51 +0200

    overflow warning added

--- src/IntaRNA/PredictorMfeEns.cpp
+++ src/IntaRNA/PredictorMfeEns.cpp
@@ -57,9 +57,14 @@ updateZ( const size_t i1, const size_t j1
 		, const Z_type partZ
 		, const bool isHybridZ )
 {
+	// check if something to be done
+	if (Z_equal(partZ,0) || Z_isINF(overallZhybrid))
+		return;
 	// update overall hybridization partition function
 	if (isHybridZ) {
 		overallZhybrid += partZ;
+		if (Z_isINF(overallZhybrid))
+			LOG(WARNING) <<"!!! PredictorMfeEns::overallZhybrid = infinity (partition function overflow)" ;
 	} else {
 		// remove ED, dangling end contributions, etc. before adding
 		overallZhybrid += ( partZ / energy.getBoltzmannWeight(energy.getE(i1,j1,i2,j2, E_type(0))) );
